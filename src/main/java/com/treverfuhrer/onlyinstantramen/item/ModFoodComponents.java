8fcc73ddf3709b73a6f10502eec53ed0aeb4d876
toki <clevertrever31@gmail.com>
2025-06-14 01:08:13 -0500

    Added more foods and item groups

--- src/main/java/com/treverfuhrer/onlyinstantramen/item/ModFoodComponents.java
+++ src/main/java/com/treverfuhrer/onlyinstantramen/item/ModFoodComponents.java
@@ -8,8 +8,11 @@ import net.minecraft.item.Items;
 public class ModFoodComponents {
 
     public static final FoodComponent NOODLES;
-    public static final FoodComponent DEFAULT_RAMEN;
+    public static final FoodComponent RAMEN;
+    public static final FoodComponent COOKED_RAMEN;
     public static final FoodComponent BEEF_RAMEN;
+    public static final FoodComponent COOKED_BEEF_RAMEN;
+
 
     public ModFoodComponents() {}
 
@@ -23,7 +26,9 @@ public class ModFoodComponents {
 
     static {
         NOODLES = (new FoodComponent.Builder()).nutrition(1).saturationModifier(0.1f).build();
-        DEFAULT_RAMEN = (new FoodComponent.Builder()).nutrition(7).saturationModifier(0.8f).usingConvertsTo(Items.BOWL).build();
-        BEEF_RAMEN = createRamen(9, 1.0f, new StatusEffectInstance(StatusEffects.STRENGTH, 20 * 20, 0), 1.0f).build();
+        RAMEN = (new FoodComponent.Builder()).nutrition(2).saturationModifier(0.2f).usingConvertsTo(Items.BOWL).build();
+        COOKED_RAMEN = (new FoodComponent.Builder()).nutrition(7).saturationModifier(0.8f).usingConvertsTo(Items.BOWL).build();
+        BEEF_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.STRENGTH, 300, 0), 1.0f).build();
+        COOKED_BEEF_RAMEN = createRamen(9, 1.0f, new StatusEffectInstance(StatusEffects.STRENGTH, 3600, 0), 1.0f).build();
     }
 }
--- src/main/java/com/treverfuhrer/onlyinstantramen/item/ModItems.java
+++ src/main/java/com/treverfuhrer/onlyinstantramen/item/ModItems.java
@@ -2,7 +2,11 @@ package com.treverfuhrer.onlyinstantramen.item;
 
 import com.treverfuhrer.onlyinstantramen.OnlyInstantRamen;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -12,22 +16,38 @@ public final class ModItems {
     private ModItems() {}
  
     // Items
-    public static final Item NOODLES = register("noodles", 
-        new Item(new Item.Settings().maxCount(1).food(ModFoodComponents.NOODLES)));
+    public static final Item NOODLES = registerRamen("noodles", ModFoodComponents.NOODLES);
 
-    public static final Item DEFAULT_RAMEN = register("default_ramen", 
-        new Item(new Item.Settings().maxCount(1).food(ModFoodComponents.DEFAULT_RAMEN)));
+    public static final Item RAMEN = registerRamen("ramen", ModFoodComponents.RAMEN);
+    public static final Item COOKED_RAMEN = registerRamen("cooked_ramen", ModFoodComponents.COOKED_RAMEN);
 
-    public static final Item BEEF_RAMEN = register("beef_ramen", 
-        new Item(new Item.Settings().maxCount(1).food(ModFoodComponents.BEEF_RAMEN)));
+    public static final Item BEEF_RAMEN = registerRamen("beef_ramen", ModFoodComponents.BEEF_RAMEN);
+    public static final Item COOKED_BEEF_RAMEN = registerRamen("cooked_beef_ramen", ModFoodComponents.COOKED_BEEF_RAMEN);
  
+    // Helper methods
+    public static Item registerRamen(String name,  FoodComponent component) {
+        return Registry.register(Registries.ITEM, Identifier.of(OnlyInstantRamen.MOD_ID, name), 
+            new Item(new Item.Settings().maxCount(1).food(component)));
+    }
 
-    // Helper
     public static <T extends Item> T register(String name, T item) {
         return Registry.register(Registries.ITEM, Identifier.of(OnlyInstantRamen.MOD_ID, name), item);
     }
+
+    // Item Groups
+    public static void registerToVanillaItemGroups() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(content -> {
+            content.add(ModItems.NOODLES);
+            content.add(ModItems.RAMEN);
+            content.add(ModItems.COOKED_RAMEN);
+            content.add(ModItems.BEEF_RAMEN);
+            content.add(ModItems.COOKED_BEEF_RAMEN);
+        });
+    }
  
+    // Initialize on startup
     public static void initialize() {
         OnlyInstantRamen.LOGGER.info("Registering Mod Items for " + OnlyInstantRamen.MOD_ID);
+        registerToVanillaItemGroups();
     }
 }

95f6307e5a64faaaf5540baba870dfe3e6991a12
toki <clevertrever31@gmail.com>
2025-06-17 03:04:44 -0500

    Added food components and item registeration for all ramen items

--- src/main/java/com/treverfuhrer/onlyinstantramen/item/ModFoodComponents.java
+++ src/main/java/com/treverfuhrer/onlyinstantramen/item/ModFoodComponents.java
@@ -10,8 +10,95 @@ public class ModFoodComponents {
     public static final FoodComponent NOODLES;
     public static final FoodComponent RAMEN;
     public static final FoodComponent COOKED_RAMEN;
+
+    // Strength
     public static final FoodComponent BEEF_RAMEN;
     public static final FoodComponent COOKED_BEEF_RAMEN;
+    public static final FoodComponent BRUTE_RAMEN;
+    public static final FoodComponent COOKED_BRUTE_RAMEN;
+
+    // Regeneration
+    public static final FoodComponent HERBAL_RAMEN;
+    public static final FoodComponent COOKED_HERBAL_RAMEN;
+    public static final FoodComponent GOLDENLEAF_RAMEN;
+    public static final FoodComponent COOKED_GOLDENLEAF_RAMEN;
+
+    // Night Vision
+    public static final FoodComponent GLOWBERRY_RAMEN;
+    public static final FoodComponent COOKED_GLOWBERRY_RAMEN;
+    public static final FoodComponent FOX_RAMEN;
+    public static final FoodComponent COOKED_FOX_RAMEN;
+
+    // Jump Boost
+    public static final FoodComponent RABBIT_RAMEN;
+    public static final FoodComponent COOKED_RABBIT_RAMEN;
+    public static final FoodComponent LEAPING_RAMEN;
+    public static final FoodComponent COOKED_LEAPING_RAMEN;
+
+    // Slow Falling
+    public static final FoodComponent FEATHER_RAMEN;
+    public static final FoodComponent COOKED_FEATHER_RAMEN;
+    public static final FoodComponent CLOUD_RAMEN;
+    public static final FoodComponent COOKED_CLOUD_RAMEN;
+
+    // Fire Resistance
+    public static final FoodComponent SPICY_RAMEN;
+    public static final FoodComponent COOKED_SPICY_RAMEN;
+    public static final FoodComponent BLAZING_RAMEN;
+    public static final FoodComponent COOKED_BLAZING_RAMEN;
+
+    // Water Breathing
+    public static final FoodComponent TURTLE_RAMEN;
+    public static final FoodComponent COOKED_TURTLE_RAMEN;
+    public static final FoodComponent DEEPSEA_RAMEN;
+    public static final FoodComponent COOKED_DEEPSEA_RAMEN;
+
+    // Swiftness
+    public static final FoodComponent CARROT_RAMEN;
+    public static final FoodComponent COOKED_CARROT_RAMEN;
+    public static final FoodComponent SWIFT_RAMEN;
+    public static final FoodComponent COOKED_SWIFT_RAMEN;
+
+    // Healing
+    public static final FoodComponent MEND_RAMEN;
+    public static final FoodComponent COOKED_MEND_RAMEN;
+    public static final FoodComponent REVIVE_RAMEN;
+    public static final FoodComponent COOKED_REVIVE_RAMEN;
+
+    // Invisibility
+    public static final FoodComponent FAINT_RAMEN;
+    public static final FoodComponent COOKED_FAINT_RAMEN;
+    public static final FoodComponent GHOST_RAMEN;
+    public static final FoodComponent COOKED_GHOST_RAMEN;
+
+    // Resistance
+    public static final FoodComponent STONE_RAMEN;
+    public static final FoodComponent COOKED_STONE_RAMEN;
+    public static final FoodComponent IRON_RAMEN;
+    public static final FoodComponent COOKED_IRON_RAMEN;
+
+    // Harming
+    public static final FoodComponent BURNED_RAMEN;
+
+    // Weakness
+    public static final FoodComponent SPOILED_RAMEN;
+
+    // NAUSEA
+    public static final FoodComponent MOLDY_RAMEN;
+
+    // POISON
+    public static final FoodComponent ROTTEN_RAMEN;
+
+    // Slowness
+    public static final FoodComponent THICK_RAMEN;
+
+    // Extra
+    public static final FoodComponent TINKER_RAMEN; // Haste 1
+    public static final FoodComponent MINERS_RAMEN; // Haste 2
+    public static final FoodComponent DRIFT_RAMEN; // Levetation 1
+    public static final FoodComponent SHULKER_RAMEN; // Levetation 2
+    public static final FoodComponent LUCKY_RAMEN; // Luck 1
+    public static final FoodComponent COOKED_LUCKY_RAMEN; // Luck 2
 
 
     public ModFoodComponents() {}
@@ -28,7 +115,95 @@ public class ModFoodComponents {
         NOODLES = (new FoodComponent.Builder()).nutrition(1).saturationModifier(0.1f).build();
         RAMEN = (new FoodComponent.Builder()).nutrition(2).saturationModifier(0.2f).usingConvertsTo(Items.BOWL).build();
         COOKED_RAMEN = (new FoodComponent.Builder()).nutrition(7).saturationModifier(0.8f).usingConvertsTo(Items.BOWL).build();
+
+        // Strength
         BEEF_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.STRENGTH, 300, 0), 1.0f).build();
-        COOKED_BEEF_RAMEN = createRamen(9, 1.0f, new StatusEffectInstance(StatusEffects.STRENGTH, 3600, 0), 1.0f).build();
+        COOKED_BEEF_RAMEN = createRamen(8, 0.9f, new StatusEffectInstance(StatusEffects.STRENGTH, 3600, 0), 1.0f).build();
+        BRUTE_RAMEN = createRamen(5, 0.5f, new StatusEffectInstance(StatusEffects.STRENGTH, 300, 1), 1.0f).build();
+        COOKED_BRUTE_RAMEN = createRamen(9, 1.0f, new StatusEffectInstance(StatusEffects.STRENGTH, 3600, 1), 1.0f).build();
+
+        // Regeneration
+        HERBAL_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.REGENERATION, 200, 0), 1.0f).build();
+        COOKED_HERBAL_RAMEN = createRamen(7, 0.8f, new StatusEffectInstance(StatusEffects.REGENERATION, 1200, 0), 1.0f).build();
+        GOLDENLEAF_RAMEN = createRamen(5, 0.5f, new StatusEffectInstance(StatusEffects.REGENERATION, 200, 1), 1.0f).build();
+        COOKED_GOLDENLEAF_RAMEN = createRamen(9, 1.0f, new StatusEffectInstance(StatusEffects.REGENERATION, 1200, 1), 1.0f).build();
+
+        // Night Vision
+        GLOWBERRY_RAMEN = createRamen(3, 0.3f, new StatusEffectInstance(StatusEffects.NIGHT_VISION, 300, 0), 1.0f).build();
+        COOKED_GLOWBERRY_RAMEN = createRamen(6, 0.6f, new StatusEffectInstance(StatusEffects.NIGHT_VISION, 1800, 0), 1.0f).build();
+        FOX_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.NIGHT_VISION, 300, 1), 1.0f).build();
+        COOKED_FOX_RAMEN = createRamen(8, 0.8f, new StatusEffectInstance(StatusEffects.NIGHT_VISION, 1800, 1), 1.0f).build();
+
+        // Jump Boost
+        RABBIT_RAMEN = createRamen(3, 0.3f, new StatusEffectInstance(StatusEffects.JUMP_BOOST, 300, 0), 1.0f).build();
+        COOKED_RABBIT_RAMEN = createRamen(7, 0.7f, new StatusEffectInstance(StatusEffects.JUMP_BOOST, 1800, 0), 1.0f).build();
+        LEAPING_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.JUMP_BOOST, 300, 1), 1.0f).build();
+        COOKED_LEAPING_RAMEN = createRamen(8, 0.8f, new StatusEffectInstance(StatusEffects.JUMP_BOOST, 1800, 1), 1.0f).build();
+
+        // Slow Falling
+        FEATHER_RAMEN = createRamen(3, 0.3f, new StatusEffectInstance(StatusEffects.SLOW_FALLING, 300, 0), 1.0f).build();
+        COOKED_FEATHER_RAMEN = createRamen(7, 0.7f, new StatusEffectInstance(StatusEffects.SLOW_FALLING, 1800, 0), 1.0f).build();
+        CLOUD_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.SLOW_FALLING, 300, 1), 1.0f).build();
+        COOKED_CLOUD_RAMEN = createRamen(8, 0.8f, new StatusEffectInstance(StatusEffects.SLOW_FALLING, 1800, 1), 1.0f).build();
+
+        // Fire Resistance
+        SPICY_RAMEN = createRamen(3, 0.3f, new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 300, 0), 1.0f).build();
+        COOKED_SPICY_RAMEN = createRamen(6, 0.6f, new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 1800, 0), 1.0f).build();
+        BLAZING_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 300, 1), 1.0f).build();
+        COOKED_BLAZING_RAMEN = createRamen(8, 0.8f, new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 1800, 1), 1.0f).build();
+
+        // Water Breathing
+        TURTLE_RAMEN = createRamen(3, 0.3f, new StatusEffectInstance(StatusEffects.WATER_BREATHING, 300, 0), 1.0f).build();
+        COOKED_TURTLE_RAMEN = createRamen(6, 0.6f, new StatusEffectInstance(StatusEffects.WATER_BREATHING, 1800, 0), 1.0f).build();
+        DEEPSEA_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.WATER_BREATHING, 300, 1), 1.0f).build();
+        COOKED_DEEPSEA_RAMEN = createRamen(8, 0.8f, new StatusEffectInstance(StatusEffects.WATER_BREATHING, 1800, 1), 1.0f).build();
+
+        // Swiftness
+        CARROT_RAMEN = createRamen(3, 0.3f, new StatusEffectInstance(StatusEffects.SPEED, 300, 0), 1.0f).build();
+        COOKED_CARROT_RAMEN = createRamen(6, 0.6f, new StatusEffectInstance(StatusEffects.SPEED, 1800, 0), 1.0f).build();
+        SWIFT_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.SPEED, 300, 1), 1.0f).build();
+        COOKED_SWIFT_RAMEN = createRamen(8, 0.8f, new StatusEffectInstance(StatusEffects.SPEED, 1800, 1), 1.0f).build();
+
+        // Healing
+        MEND_RAMEN = createRamen(3, 0.3f, new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 0), 1.0f).build();
+        COOKED_MEND_RAMEN = createRamen(6, 0.6f, new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 0), 1.0f).build();
+        REVIVE_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 1), 1.0f).build();
+        COOKED_REVIVE_RAMEN = createRamen(8, 0.8f, new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 1), 1.0f).build();
+
+        // Invisibility
+        FAINT_RAMEN = createRamen(3, 0.3f, new StatusEffectInstance(StatusEffects.INVISIBILITY, 300, 0), 1.0f).build();
+        COOKED_FAINT_RAMEN = createRamen(6, 0.6f, new StatusEffectInstance(StatusEffects.INVISIBILITY, 1800, 0), 1.0f).build();
+        GHOST_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.INVISIBILITY, 300, 1), 1.0f).build();
+        COOKED_GHOST_RAMEN = createRamen(8, 0.8f, new StatusEffectInstance(StatusEffects.INVISIBILITY, 1800, 1), 1.0f).build();
+
+        // Resistance
+        STONE_RAMEN = createRamen(3, 0.3f, new StatusEffectInstance(StatusEffects.RESISTANCE, 300, 0), 1.0f).build();
+        COOKED_STONE_RAMEN = createRamen(6, 0.6f, new StatusEffectInstance(StatusEffects.RESISTANCE, 1800, 0), 1.0f).build();
+        IRON_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.RESISTANCE, 300, 1), 1.0f).build();
+        COOKED_IRON_RAMEN = createRamen(8, 0.8f, new StatusEffectInstance(StatusEffects.RESISTANCE, 1800, 1), 1.0f).build();
+
+        // Extra
+        TINKER_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.HASTE, 300, 0), 1.0f).build();
+        MINERS_RAMEN = createRamen(8, 0.8f, new StatusEffectInstance(StatusEffects.HASTE, 1800, 1), 1.0f).build();
+        DRIFT_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.LEVITATION, 200, 0), 1.0f).build();
+        SHULKER_RAMEN = createRamen(8, 0.8f, new StatusEffectInstance(StatusEffects.LEVITATION, 400, 1), 1.0f).build();
+        LUCKY_RAMEN = createRamen(4, 0.4f, new StatusEffectInstance(StatusEffects.LUCK, 300, 0), 1.0f).build();
+        COOKED_LUCKY_RAMEN = createRamen(8, 0.8f, new StatusEffectInstance(StatusEffects.LUCK, 1800, 0), 1.0f).build();
+
+        // Harming
+        BURNED_RAMEN = createRamen(3, 0.3f, new StatusEffectInstance(StatusEffects.INSTANT_DAMAGE, 1, 0), 1.0f).build();        
+
+        // Slowness
+        THICK_RAMEN = createRamen(2, 0.2f, new StatusEffectInstance(StatusEffects.SLOWNESS, 300, 0), 1.0f).build();
+
+        // WEAKNESS
+        SPOILED_RAMEN = createRamen(2, 0.1f, new StatusEffectInstance(StatusEffects.WEAKNESS, 300, 0), 1.0f).build();
+        
+        // NAUSEA
+        MOLDY_RAMEN = createRamen(2, 0.1f, new StatusEffectInstance(StatusEffects.NAUSEA, 200, 0), 1.0f).build();
+        
+        // POISON
+        ROTTEN_RAMEN = createRamen(2, 0.1f, new StatusEffectInstance(StatusEffects.POISON, 200, 0), 1.0f).build();
+
     }
 }
--- src/main/java/com/treverfuhrer/onlyinstantramen/item/ModItems.java
+++ src/main/java/com/treverfuhrer/onlyinstantramen/item/ModItems.java
@@ -17,12 +17,82 @@ public final class ModItems {
  
     // Items
     public static final Item NOODLES = registerRamen("noodles", ModFoodComponents.NOODLES);
-
     public static final Item RAMEN = registerRamen("ramen", ModFoodComponents.RAMEN);
     public static final Item COOKED_RAMEN = registerRamen("cooked_ramen", ModFoodComponents.COOKED_RAMEN);
 
+    // Strength
     public static final Item BEEF_RAMEN = registerRamen("beef_ramen", ModFoodComponents.BEEF_RAMEN);
     public static final Item COOKED_BEEF_RAMEN = registerRamen("cooked_beef_ramen", ModFoodComponents.COOKED_BEEF_RAMEN);
+    public static final Item BRUTE_RAMEN = registerRamen("brute_ramen", ModFoodComponents.BRUTE_RAMEN);
+    public static final Item COOKED_BRUTE_RAMEN = registerRamen("cooked_brute_ramen", ModFoodComponents.COOKED_BRUTE_RAMEN);
+
+    // Regeneration
+    public static final Item HERBAL_RAMEN = registerRamen("herbal_ramen", ModFoodComponents.HERBAL_RAMEN);
+    public static final Item COOKED_HERBAL_RAMEN = registerRamen("cooked_herbal_ramen", ModFoodComponents.COOKED_HERBAL_RAMEN);
+    public static final Item GOLDENLEAF_RAMEN = registerRamen("goldenleaf_ramen", ModFoodComponents.GOLDENLEAF_RAMEN);
+    public static final Item COOKED_GOLDENLEAF_RAMEN = registerRamen("cooked_goldenleaf_ramen", ModFoodComponents.COOKED_GOLDENLEAF_RAMEN);
+
+    // Night Vision
+    public static final Item GLOWBERRY_RAMEN = registerRamen("glowberry_ramen", ModFoodComponents.GLOWBERRY_RAMEN);
+    public static final Item COOKED_GLOWBERRY_RAMEN = registerRamen("cooked_glowberry_ramen", ModFoodComponents.COOKED_GLOWBERRY_RAMEN);
+    public static final Item FOX_RAMEN = registerRamen("fox_ramen", ModFoodComponents.FOX_RAMEN);
+    public static final Item COOKED_FOX_RAMEN = registerRamen("cooked_fox_ramen", ModFoodComponents.COOKED_FOX_RAMEN);
+
+    // Jump Boost
+    public static final Item RABBIT_RAMEN = registerRamen("rabbit_ramen", ModFoodComponents.RABBIT_RAMEN);
+    public static final Item COOKED_RABBIT_RAMEN = registerRamen("cooked_rabbit_ramen", ModFoodComponents.COOKED_RABBIT_RAMEN);
+    public static final Item LEAPING_RAMEN = registerRamen("leaping_ramen", ModFoodComponents.LEAPING_RAMEN);
+    public static final Item COOKED_LEAPING_RAMEN = registerRamen("cooked_leaping_ramen", ModFoodComponents.COOKED_LEAPING_RAMEN);
+
+    // Slow Falling
+    public static final Item FEATHER_RAMEN = registerRamen("feather_ramen", ModFoodComponents.FEATHER_RAMEN);
+    public static final Item COOKED_FEATHER_RAMEN = registerRamen("cooked_feather_ramen", ModFoodComponents.COOKED_FEATHER_RAMEN);
+    public static final Item CLOUD_RAMEN = registerRamen("cloud_ramen", ModFoodComponents.CLOUD_RAMEN);
+    public static final Item COOKED_CLOUD_RAMEN = registerRamen("cooked_cloud_ramen", ModFoodComponents.COOKED_CLOUD_RAMEN);
+
+    // Fire Resistance
+    public static final Item SPICY_RAMEN = registerRamen("spicy_ramen", ModFoodComponents.SPICY_RAMEN);
+    public static final Item COOKED_SPICY_RAMEN = registerRamen("cooked_spicy_ramen", ModFoodComponents.COOKED_SPICY_RAMEN);
+    public static final Item BLAZING_RAMEN = registerRamen("blazing_ramen", ModFoodComponents.BLAZING_RAMEN);
+    public static final Item COOKED_BLAZING_RAMEN = registerRamen("cooked_blazing_ramen", ModFoodComponents.COOKED_BLAZING_RAMEN);
+
+    // Water Breathing
+    public static final Item TURTLE_RAMEN = registerRamen("turtle_ramen", ModFoodComponents.TURTLE_RAMEN);
+    public static final Item COOKED_TURTLE_RAMEN = registerRamen("cooked_turtle_ramen", ModFoodComponents.COOKED_TURTLE_RAMEN);
+    public static final Item DEEPSEA_RAMEN = registerRamen("deepsea_ramen", ModFoodComponents.DEEPSEA_RAMEN);
+    public static final Item COOKED_DEEPSEA_RAMEN = registerRamen("cooked_deepsea_ramen", ModFoodComponents.COOKED_DEEPSEA_RAMEN);
+
+    // Swiftness
+    public static final Item CARROT_RAMEN = registerRamen("carrot_ramen", ModFoodComponents.CARROT_RAMEN);
+    public static final Item COOKED_CARROT_RAMEN = registerRamen("cooked_carrot_ramen", ModFoodComponents.COOKED_CARROT_RAMEN);
+    public static final Item SWIFT_RAMEN = registerRamen("swift_ramen", ModFoodComponents.SWIFT_RAMEN);
+    public static final Item COOKED_SWIFT_RAMEN = registerRamen("cooked_swift_ramen", ModFoodComponents.COOKED_SWIFT_RAMEN);
+
+    // Healing
+    public static final Item MEND_RAMEN = registerRamen("mend_ramen", ModFoodComponents.MEND_RAMEN);
+    public static final Item COOKED_MEND_RAMEN = registerRamen("cooked_mend_ramen", ModFoodComponents.COOKED_MEND_RAMEN);
+    public static final Item REVIVE_RAMEN = registerRamen("revive_ramen", ModFoodComponents.REVIVE_RAMEN);
+    public static final Item COOKED_REVIVE_RAMEN = registerRamen("cooked_revive_ramen", ModFoodComponents.COOKED_REVIVE_RAMEN);
+
+    // Invisibility
+    public static final Item FAINT_RAMEN = registerRamen("faint_ramen", ModFoodComponents.FAINT_RAMEN);
+    public static final Item COOKED_FAINT_RAMEN = registerRamen("cooked_faint_ramen", ModFoodComponents.COOKED_FAINT_RAMEN);
+    public static final Item GHOST_RAMEN = registerRamen("ghost_ramen", ModFoodComponents.GHOST_RAMEN);
+    public static final Item COOKED_GHOST_RAMEN = registerRamen("cooked_ghost_ramen", ModFoodComponents.COOKED_GHOST_RAMEN);
+
+    // Resistance
+    public static final Item STONE_RAMEN = registerRamen("stone_ramen", ModFoodComponents.STONE_RAMEN);
+    public static final Item COOKED_STONE_RAMEN = registerRamen("cooked_stone_ramen", ModFoodComponents.COOKED_STONE_RAMEN);
+    public static final Item IRON_RAMEN = registerRamen("iron_ramen", ModFoodComponents.IRON_RAMEN);
+    public static final Item COOKED_IRON_RAMEN = registerRamen("cooked_iron_ramen", ModFoodComponents.COOKED_IRON_RAMEN);
+
+    // Negative Effects
+    public static final Item BURNED_RAMEN = registerRamen("burned_ramen", ModFoodComponents.BURNED_RAMEN);
+    public static final Item SPOILED_RAMEN = registerRamen("spoiled_ramen", ModFoodComponents.SPOILED_RAMEN);
+    public static final Item MOLDY_RAMEN = registerRamen("moldy_ramen", ModFoodComponents.MOLDY_RAMEN);
+    public static final Item ROTTEN_RAMEN = registerRamen("rotten_ramen", ModFoodComponents.ROTTEN_RAMEN);
+    public static final Item THICK_RAMEN = registerRamen("thick_ramen", ModFoodComponents.THICK_RAMEN);
+
  
     // Helper methods
     public static Item registerRamen(String name,  FoodComponent component) {
@@ -37,13 +107,63 @@ public final class ModItems {
     // Item Groups
     public static void registerToVanillaItemGroups() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(content -> {
-            content.addAfter(Items.RABBIT_STEW, ModItems.NOODLES);
-            content.addAfter(ModItems.NOODLES, ModItems.RAMEN);
-            content.addAfter(ModItems.RAMEN, ModItems.COOKED_RAMEN);
-            content.addAfter(ModItems.COOKED_RAMEN, ModItems.BEEF_RAMEN);
-            content.addAfter(ModItems.BEEF_RAMEN, ModItems.COOKED_BEEF_RAMEN);
+            content.addAfter(Items.SPIDER_EYE, NOODLES);
+
+            // Reverse order for correct appearance
+            content.addAfter(NOODLES, THICK_RAMEN);
+            content.addAfter(NOODLES, ROTTEN_RAMEN);
+            content.addAfter(NOODLES, MOLDY_RAMEN);
+            content.addAfter(NOODLES, SPOILED_RAMEN);
+            content.addAfter(NOODLES, BURNED_RAMEN);
+            content.addAfter(NOODLES, COOKED_IRON_RAMEN);
+            content.addAfter(NOODLES, IRON_RAMEN);
+            content.addAfter(NOODLES, COOKED_STONE_RAMEN);
+            content.addAfter(NOODLES, STONE_RAMEN);
+            content.addAfter(NOODLES, COOKED_GHOST_RAMEN);
+            content.addAfter(NOODLES, GHOST_RAMEN);
+            content.addAfter(NOODLES, COOKED_FAINT_RAMEN);
+            content.addAfter(NOODLES, FAINT_RAMEN);
+            content.addAfter(NOODLES, COOKED_REVIVE_RAMEN);
+            content.addAfter(NOODLES, REVIVE_RAMEN);
+            content.addAfter(NOODLES, COOKED_MEND_RAMEN);
+            content.addAfter(NOODLES, MEND_RAMEN);
+            content.addAfter(NOODLES, COOKED_SWIFT_RAMEN);
+            content.addAfter(NOODLES, SWIFT_RAMEN);
+            content.addAfter(NOODLES, COOKED_CARROT_RAMEN);
+            content.addAfter(NOODLES, CARROT_RAMEN);
+            content.addAfter(NOODLES, COOKED_DEEPSEA_RAMEN);
+            content.addAfter(NOODLES, DEEPSEA_RAMEN);
+            content.addAfter(NOODLES, COOKED_TURTLE_RAMEN);
+            content.addAfter(NOODLES, TURTLE_RAMEN);
+            content.addAfter(NOODLES, COOKED_BLAZING_RAMEN);
+            content.addAfter(NOODLES, BLAZING_RAMEN);
+            content.addAfter(NOODLES, COOKED_SPICY_RAMEN);
+            content.addAfter(NOODLES, SPICY_RAMEN);
+            content.addAfter(NOODLES, COOKED_CLOUD_RAMEN);
+            content.addAfter(NOODLES, CLOUD_RAMEN);
+            content.addAfter(NOODLES, COOKED_FEATHER_RAMEN);
+            content.addAfter(NOODLES, FEATHER_RAMEN);
+            content.addAfter(NOODLES, COOKED_LEAPING_RAMEN);
+            content.addAfter(NOODLES, LEAPING_RAMEN);
+            content.addAfter(NOODLES, COOKED_RABBIT_RAMEN);
+            content.addAfter(NOODLES, RABBIT_RAMEN);
+            content.addAfter(NOODLES, COOKED_FOX_RAMEN);
+            content.addAfter(NOODLES, FOX_RAMEN);
+            content.addAfter(NOODLES, COOKED_GLOWBERRY_RAMEN);
+            content.addAfter(NOODLES, GLOWBERRY_RAMEN);
+            content.addAfter(NOODLES, COOKED_GOLDENLEAF_RAMEN);
+            content.addAfter(NOODLES, GOLDENLEAF_RAMEN);
+            content.addAfter(NOODLES, COOKED_HERBAL_RAMEN);
+            content.addAfter(NOODLES, HERBAL_RAMEN);
+            content.addAfter(NOODLES, COOKED_BRUTE_RAMEN);
+            content.addAfter(NOODLES, BRUTE_RAMEN);
+            content.addAfter(NOODLES, COOKED_BEEF_RAMEN);
+            content.addAfter(NOODLES, BEEF_RAMEN);
+            content.addAfter(NOODLES, COOKED_RAMEN);
+            content.addAfter(NOODLES, RAMEN);
         });
     }
+
  
     // Initialize on startup
     public static void initialize() {
